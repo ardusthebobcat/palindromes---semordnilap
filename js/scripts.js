2b72460913df06fc9ccdf92c12e3eec7847bbe3e
Michael Summers & iman mafi <student@epicodus.com>
2015-09-09 10:21:08 -0700

    second spec and provided loop logic to actually test for palindromes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
 var palindrome = function(pd) {
-  var output = true;
+  var output = false;
+  var length = pd.length
+  var left;
+  var right;
+
+  if (length % 2 === 0) {
+    left = Math.floor(length/2);
+    right = Math.ceil(length/2);
+  } else { // if odd, sets both left and right to middle point
+    left = Math.ceil((pd.length)/2);
+    right = left;
+  }
+
+  for (var count = 0, looplength = Math.floor(length/2); count <= looplength; count++) {
+    if (pd.substring(left, left-1) != pd.substring(right,right-1)) {
+      break;
+    }
+
+    if (left === 1) {
+      output = true;
+    }
+
+    left -= 1;
+    right += 1;
+  }
+
   return output;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe("palindromes", function () {
   it("checks a 1 digit and declares it a palindrome", function() {
-    palindrome("a").should.equal(true)
+    palindrome("aaa").should.equal(true)
+  });
+
+  it("checks a clearly-not palindrome and returns false", function() {
+    palindrome("car").should.equal(false)
   });
 });
